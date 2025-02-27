4c2a7bd76ce0681c13053f839023871ccf3a35a1
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-02-27 14:19:22 +0600

    app title fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Simple Whiteboard App</h1>
+      <h1>React Konva Canvas</h1>
       <Whiteboard />
     </div>
   );
