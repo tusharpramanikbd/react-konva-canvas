8987c8fb38859c81cb281a53458be6948393f7a5
Tushar Pramanik <tusharpramanikbd@gmail.com>
2025-02-28 11:48:15 +0600

    added active button state

--- src/components/Whiteboard/Whiteboard.tsx
+++ src/components/Whiteboard/Whiteboard.tsx
@@ -2,19 +2,45 @@
 
 import { Stage, Layer } from "react-konva";
 import "./Whiteboard.css";
+import { useState } from "react";
+
+type Tool = "pen" | "eraser" | "rectangle" | "circle";
 
 const Whiteboard: React.FC = () => {
+  const [tool, setTool] = useState<Tool>("pen");
+
   // Stage dimensions
   const stageWidth = window.innerWidth - 300;
   const stageHeight = window.innerHeight - 300;
 
+  const isActive = (toolName: Tool): string => {
+    return tool === toolName ? "active" : "";
+  };
+
   return (
     <div className="whiteboard-container">
       <div className="toolbar">
-        <button>Pen</button>
-        <button>Eraser</button>
-        <button>Rectangle</button>
-        <button>Circle</button>
+        <button className={isActive("pen")} onClick={() => setTool("pen")}>
+          Pen
+        </button>
+        <button
+          className={isActive("eraser")}
+          onClick={() => setTool("eraser")}
+        >
+          Eraser
+        </button>
+        <button
+          className={isActive("rectangle")}
+          onClick={() => setTool("rectangle")}
+        >
+          Rectangle
+        </button>
+        <button
+          className={isActive("circle")}
+          onClick={() => setTool("circle")}
+        >
+          Circle
+        </button>
       </div>
 
       <Stage
